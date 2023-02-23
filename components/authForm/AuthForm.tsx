e80430e01f7f4e6acae55b22422362e6e0b91e04
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-02-23 12:53:56 +0100

    Change style in AuthForm

--- components/authForm/AuthForm.tsx
+++ components/authForm/AuthForm.tsx
@@ -19,9 +19,9 @@ const AuthForm = ({ title }: Props) => {
 			<Head>
 				<title>{isLogInRoute ? 'Login' : 'Register'}</title>
 			</Head>
-			<section className='fade-in-from-bottom relative w-[280px] overflow-hidden rounded-md bg-slate-300/80 p-5 sm:w-[310px]'>
+			<section className='fade-in-from-bottom flex w-[280px] flex-col items-center overflow-hidden rounded-md bg-slate-300/80 p-5 sm:w-[310px]'>
 				{isLoading && <LoadingSpinner />}
-				<h2 className='ml-[50%] inline-block -translate-x-1/2 border-b-2 border-indigo-500 px-2 text-center text-xl font-semibold'>
+				<h2 className='border-b-2 border-indigo-500 px-2 text-xl font-semibold'>
 					{title}
 				</h2>
 				<Form
@@ -29,7 +29,7 @@ const AuthForm = ({ title }: Props) => {
 					isLogInRoute={isLogInRoute}
 					setIsLoading={setIsLoading}
 				/>
-				<p className='inline-block border-b-2 border-indigo-500 px-2'>
+				<p className='border-b-2 border-indigo-500 px-2 text-sm'>
 					{isLogInRoute ? 'Have not account?' : 'Have account?'}
 					<Link
 						className='pl-3 transition-opacity hover:opacity-80'
