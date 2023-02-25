95141caf8c57ab3bc7dc4f644f6f1bddcdad1660
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-02-25 14:41:20 +0100

    Add useResponseMessage hook

---
+++ hooks/useResponseMessage.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+interface Response {
+	status: 'success' | 'error';
+	message: string;
+}
+
+interface Props {
+	style: string;
+	colors: {
+		success: string;
+		error: string;
+	};
+}
+
+const useResponseMessage = ({ style, colors }: Props) => {
+	const [response, setResponse] = useState<Response | null>();
+
+	const responseColor =
+		response?.status === 'success' ? `${colors.success}` : `${colors.error}`;
+
+	const message = (
+		<span
+			className={`block text-center font-semibold ${responseColor} ${style}`}
+		>
+			{response?.message}
+		</span>
+	);
+
+	useEffect(() => {
+		if (response?.status === 'success') {
+			const timeout = setTimeout(() => {
+				setResponse(null);
+			}, 2000);
+
+			return () => clearTimeout(timeout);
+		}
+	}, [response]);
+
+	return { message, response, setResponse };
+};
+
+export default useResponseMessage;
