1c4baa214efe6a0e9d0bc59fec5675e2d513c0cc
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-03-03 14:27:32 +0100

    Add change image component and api route

---
+++ components/settings/image/Image.tsx
@@ -0,0 +1,127 @@
+import Button from '@/components/UI/button/Button';
+import ErrorMessage from '@/components/UI/errorMessage/ErrorMessage';
+import { storage } from '@/firebase.config';
+import {
+	availableImageFormats,
+	handleDataChange,
+	checkIfFileIsAnImage,
+	MAX_IMAGE_SIZE_IN_MB,
+} from '@/helpers';
+import { Response } from '@/interface';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { useEffect, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
+
+interface Props {
+	setResponse: (data: Response) => void;
+}
+
+interface Input {
+	file: FileList;
+}
+
+const Image = ({ setResponse }: Props) => {
+	const [imageName, setImageName] = useState('');
+	const [imagePath, setimagePath] = useState<string>();
+	const [parent] = useAutoAnimate();
+
+	const {
+		register,
+		handleSubmit,
+		reset,
+		getValues,
+		formState: { errors, isSubmitSuccessful },
+	} = useForm<Input>({
+		mode: 'onTouched',
+		defaultValues: {
+			file: undefined,
+		},
+	});
+
+	const changeUserImage: SubmitHandler<Input> = ({ file }) => {
+		const selectedFile = file[0];
+
+		if (selectedFile.size > MAX_IMAGE_SIZE_IN_MB * 1024 * 1024) {
+			return setResponse({
+				status: 'error',
+				message: `Maximum image size is ${MAX_IMAGE_SIZE_IN_MB}mb`,
+			});
+		}
+
+		const reader = new FileReader();
+
+		reader.readAsDataURL(selectedFile);
+		reader.onload = async () => {
+			const dataUrl = reader.result;
+
+			await handleDataChange(
+				'/api/settings/changeImage',
+				dataUrl,
+				(data) => setResponse({ status: 'success', message: data.message }),
+				(err) => setResponse({ status: 'error', message: err.message })
+			);
+		};
+	};
+
+	useEffect(() => {
+		setImageName('');
+		setimagePath(undefined);
+		reset({
+			file: undefined,
+		});
+	}, [isSubmitSuccessful]);
+
+	return (
+		<div ref={parent}>
+			<form
+				className='flex items-center'
+				onSubmit={handleSubmit(changeUserImage)}
+			>
+				<label
+					htmlFor='change-image'
+					className='cursor-pointer transition-opacity hover:opacity-80'
+					aria-label='Change Image'
+					title='Change Image'
+					aria-haspopup
+				>
+					<p className='max-w-[200px] px-4 py-2'>
+						{imageName ? imageName : 'Choose an image'}
+					</p>
+				</label>
+
+				<input
+					type='file'
+					accept={availableImageFormats
+						.map((format) => `image/${format}`)
+						.join(', ')}
+					id='change-image'
+					className='hidden'
+					aria-hidden
+					{...register('file', {
+						required: 'Choose some image',
+						onChange: () => {
+							setImageName('');
+							setimagePath(undefined);
+
+							const file = getValues('file')[0] as File;
+							if (file) {
+								checkIfFileIsAnImage(file, setImageName);
+								setimagePath(URL.createObjectURL(file));
+							}
+						},
+					})}
+				/>
+				<Button
+					text='Change*'
+					style='px-4 py-2 text-sm ml-auto transition-colors hover:bg-teal-500'
+				></Button>
+			</form>
+
+			<ErrorMessage message={errors.file?.message} style='text-red-500' />
+			{imagePath && <img src={imagePath} alt='Selected image' />}
+		</div>
+	);
+};
+
+export default Image;
---
+++ pages/api/settings/changeImage.ts
@@ -0,0 +1,115 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { storage } from '@/firebase.config';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { ref, getDownloadURL, uploadString } from 'firebase/storage';
+import {
+	getDataFromCollection,
+	MAX_IMAGE_SIZE_IN_MB,
+	setDataToCollection,
+	updateCollectionData,
+	updateUser,
+} from '@/helpers';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
+
+export const config = {
+	api: {
+		bodyParser: {
+			sizeLimit: `${MAX_IMAGE_SIZE_IN_MB}mb`,
+		},
+	},
+};
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	if (req.method !== 'POST') return;
+
+	const data = await getServerSession(req, res, authOptions);
+
+	const { reqBody: ImageData } = req.body as { reqBody: string };
+
+	const storageRef = ref(storage, data?.user.uid);
+
+	try {
+		const snapshot = await uploadString(storageRef, ImageData, 'data_url');
+
+		const imageUrl = await getDownloadURL(snapshot.ref);
+
+		await updateUser(data?.user.name!, imageUrl);
+
+		await updateCollectionData(
+			process.env.NEXT_PUBLIC_FIREBASE_COLLECTION_USERS!,
+			data?.user.uid!,
+			{
+				image: imageUrl,
+			}
+		);
+
+		await updateCollectionData(
+			process.env.NEXT_PUBLIC_FIREBASE_COLLECTION_USER_CHATS!,
+			data?.user.uid!,
+			{
+				[`owner.image`]: imageUrl,
+			}
+		);
+
+		const userData = await getDataFromCollection(
+			process.env.NEXT_PUBLIC_FIREBASE_COLLECTION_USER_CHATS!,
+			data?.user.uid!
+		);
+		const activeChats = userData.data()?.activeChats;
+
+		for (const sharedId in activeChats) {
+			const id = activeChats[sharedId].userData.id;
+
+			await updateCollectionData(
+				process.env.NEXT_PUBLIC_FIREBASE_COLLECTION_USER_CHATS!,
+				id,
+				{
+					[`activeChats.${sharedId}.userData.image`]: imageUrl,
+				}
+			);
+		}
+
+		for (const sharedId in activeChats) {
+			const chatCollection = await getDataFromCollection(
+				process.env.NEXT_PUBLIC_FIREBASE_COLLECTION_CHATS!,
+				sharedId
+			);
+
+			const allMessagesArr = chatCollection.data()?.messages;
+
+			const updatedMessages = allMessagesArr.map((message: any) => {
+				if (message.senderData.id === data?.user.uid!) {
+					const updatedMessage = { ...message };
+					updatedMessage.senderData.image = imageUrl;
+					return updatedMessage;
+				} else {
+					return message;
+				}
+			});
+
+			await setDataToCollection(
+				process.env.NEXT_PUBLIC_FIREBASE_COLLECTION_CHATS!,
+				sharedId,
+				{
+					messages: updatedMessages,
+				}
+			);
+		}
+
+		console.log('Success');
+		res.status(200).json({
+			status: 'success',
+			message: 'Image has been changed',
+		});
+	} catch (err: any) {
+		console.log(err);
+		res.status(500).json({
+			status: 'error',
+			errorMessage: 'Something went wrong, try later',
+		});
+	}
+}
