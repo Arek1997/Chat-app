25e47911be39f9d392aea122ea547eedef0b99cf
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-03-07 13:37:21 +0100

    Add property to interface, add condictionaly 'p' and 'image' render

--- components/chat/message/Message.tsx
+++ components/chat/message/Message.tsx
@@ -1,23 +1,30 @@
 interface Props {
-	message: string;
 	owner: boolean;
-	user?: string;
+	message?: string;
 	image?: string;
+	userName?: string;
+	userImage?: string;
 }
 
-const Message = ({ message, owner, image, user }: Props) => {
-	const userImage = image || '/person-icon.png';
-	const userName = user || 'User Name';
-
+const Message = ({ message, owner, userImage, userName, image }: Props) => {
 	return (
 		<li className={`message ${owner ? 'message-owner' : ''} max-w-[80%]`}>
 			<article className='flex items-end gap-x-3 '>
 				<img
-					src={userImage}
-					alt={userName}
+					src={userImage || '/person-icon.png'}
+					alt={userName || 'User Name'}
 					className='h-6 w-6 rounded-full object-cover'
 				/>
-				<p className='rounded-lg bg-slate-100/20 p-3 text-left'>{message}</p>
+
+				<div>
+					{message && (
+						<p className='rounded-lg bg-slate-100/20 p-3 text-left'>
+							{message}
+						</p>
+					)}
+
+					{image && <img src={image} className='mt-4' />}
+				</div>
 			</article>
 		</li>
 	);
